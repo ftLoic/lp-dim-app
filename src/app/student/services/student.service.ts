1c3d4dffa0ce57d2271275902d9db9d2c97731a2
Loïc <36301183+ftLoic@users.noreply.github.com>
2022-03-21 13:12:16 +0100

    test

---
+++ src/app/student/services/student.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Student } from 'src/app/core/models/student';
+import { environment } from 'src/environments/environment';
+
+@Injectable()
+export class StudentService {
+
+  private readonly studentPath: string = "/students";
+
+  constructor(private _http: HttpClient) { }
+
+    get(): Observable<Student[]> {
+        return this._http.get<Student[]>(`${environment.apiBaseUrl}${this.studentPath}`);
+    }
+
+    getStudentById(id: number): Observable<Student>{
+        return this._http.get<Student>(`${environment.apiBaseUrl}${this.studentPath}/${id}`);
+    } 
+
+    createStudent(student: Student): Observable<Student>{
+        return this._http.post<Student>(`${environment.apiBaseUrl}${this.studentPath}`, student);
+    }
+
+    updateStudent(student: Student): Observable<Student>{
+        return this._http.put<Student>(`${environment.apiBaseUrl}${this.studentPath}`, student);
+    }
+
+    deleteStudent(id: number): Observable<any>{
+        return this._http.delete(`${environment.apiBaseUrl}${this.studentPath}/${id}`);
+    }
+}
